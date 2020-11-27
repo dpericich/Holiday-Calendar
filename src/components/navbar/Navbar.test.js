68efe281f0c0c03699a44561d4f591fc249036a0
Daniel <dpericich@gmail.com>
2020-11-26 21:22:31 -0600

    used contains test on shallow render to check home button renders in navbar

--- src/components/navbar/Navbar.test.js
+++ src/components/navbar/Navbar.test.js
@@ -1,3 +1,14 @@
-test('', () => {
+import React from 'react';
+import {shallow} from 'enzyme';
+import Navbar from './Navbar';
+import HomeButton from '../home-button/HomeButton';
+
+const setup = () => {
+    return shallow(<Navbar />);
+}
+
+test('check that home button renders with the navbar', () => {
+    const wrapper = setup();
+    expect(wrapper.contains(<HomeButton />)).toBe(true);
     
 })
